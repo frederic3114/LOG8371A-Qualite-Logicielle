4e054e54f95a2ed63f5e7d88f007869d411c4d0d
Gabriel <dizco@users.noreply.github.com>
2019-02-15 13:15:23 -0500

    Add AdditionHelper

---
+++ TP1/weka/src/test/java/weka/AdditionHelperTest.java
@@ -0,0 +1,41 @@
+package weka;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+/**
+ * Tests MultiFilter. Run from the command line with: <p/>
+ * java weka.filters.MultiFilterTest
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class AdditionHelperTest extends TestCase {
+
+  public AdditionHelperTest(String name) {
+    super(name);  
+  }
+
+  public void testPositiveAddition() {
+    assertEquals(AdditionHelper.Add(1, 1), 2);
+  }
+
+  public void testNegativeAddition() {
+    assertEquals(AdditionHelper.Add(-1, -1), -2);
+  }
+
+  public void testFail() {
+    assertEquals(AdditionHelper.Add(1, 1), 0);
+  }
+
+  public static Test suite() {
+    return new TestSuite(AdditionHelperTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+
+}
